dfd85fbae66d593ec70661660bcffbbdc0f49963
BRUNAAM <140859773+BRUNAAM@users.noreply.github.com>
2023-09-15 21:54:08 -0300

    PHP

---
+++ ler_dados.php
@@ -0,0 +1,19 @@
+<?php
+// obtendo dados do formulario
+// $_GET[]
+// $_POST[]
+//_REQUEST[] obtem independente da forma
+
+$nome = $_GET['nome'];
+$idade = $_GET['idade'];
+
+echo "Seu nome é: $nome e voce tem $idade anos";
+
+
+
+
+
+
+?>
+
+
